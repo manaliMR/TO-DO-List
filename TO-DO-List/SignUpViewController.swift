4d60b5f844280b4c695bd46f80107f5f5afbc254
manali78 <manalirami78@gmail.com>
2018-09-23 20:18:37 -0400

    SignUp view added(Authentication)

---
+++ TO-DO-List/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  TO-DO-List
+//
+//  Created by Manali Rami on 2018-09-23.
+//  Copyright © 2018 Manali Rami. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func SignUp(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+            if error == nil {
+                self.performSegue(withIdentifier: "signup", sender: self)
+            }
+    }
+}
+}
