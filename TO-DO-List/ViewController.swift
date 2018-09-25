6e249af9a686b0494e2055d7ffc7f05d44d8a43a
manali78 <manalirami78@gmail.com>
2018-09-24 22:01:42 -0400

    CustomTableViewCell displayed on UI

--- TO-DO-List/ViewController.swift
+++ TO-DO-List/ViewController.swift
@@ -13,21 +13,30 @@ class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var todoLabel: UILabel!
     
 }
-class ViewController: UIViewController {
-    
-    
-    
+class ViewController: UIViewController, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "custom")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: "custom") as!
+            CustomTableViewCell
+        cell.todoLabel.text = "Hello World"
+        return cell
+    }
 }
 
